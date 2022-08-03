dd81b2f4c1d8694ac687bb652c0bb93bf277006e
Nick <cali0030@algonquinlive.com>
2022-08-03 16:39:19 -0400

    Adding intent to open CocktailActivity & Removing unneeded buttons

--- app/src/main/java/com/cst2335/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/cst2335/androidfinalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cst2335.androidfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -25,23 +26,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-    private String userInput;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.cocktail_button);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToCocktail = new Intent(MainActivity.this, CocktailActivity.class);
+                startActivity(goToCocktail);
+            }
+        });
 
     }
-
-
-
 
 }
