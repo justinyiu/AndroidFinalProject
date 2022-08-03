5362471857e17298700cb1ff3c8627413a191d97
Catherine Bui <bui00074@algonquinlive.com>
2022-08-03 19:51:50 -0400

    AndroidFinalProject

--- app/src/main/java/com/cst2335/androidfinalproject/CocktailActivity.java
+++ app/src/main/java/com/cst2335/androidfinalproject/CocktailActivity.java
@@ -2,9 +2,13 @@ package com.cst2335.androidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.preference.PreferenceManager;
 import android.view.View;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -22,8 +26,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CocktailActivity extends AppCompatActivity {
+    SharedPreferences prev = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +39,26 @@ public class CocktailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cocktail);
 
 
-
         MyHttpRequest req = new MyHttpRequest();
         req.execute("https://www.cocktaildb.com/api/json/v1/1/search.php?s=martini"); // type 1
 
+         prev = PreferenceManager.getDefaultSharedPreferences(this);
+          String previousDrink = prev.getString("PreviousDrink", "");
+          EditText drinks = findViewById(R.id.editText2);
+          drinks.setText(previousDrink);
+
+        Button search = findViewById(R.id.button);
+
+        search.setOnClickListener(bt -> savePreviousDrink(drinks.getText().toString()));
+
     }
+    private void savePreviousDrink (String previousDrink) {
+        SharedPreferences.Editor editor = prev.edit();
+        editor.putString("PreviousDrink", previousDrink);
+        editor.commit();
+    }
+
+
     private class MyHttpRequest extends AsyncTask <String, Integer, String> {
 
         public String doInBackground(String ... args) {
@@ -66,8 +89,11 @@ public class CocktailActivity extends AppCompatActivity {
                 JSONObject jsObj = new JSONObject(result);
                 JSONArray drinksArray = jsObj.getJSONArray("drinks");
                 for (int i = 0; i < drinksArray.length(); i++) {
+
                     JSONObject objectFromArray = drinksArray.getJSONObject(i);
+
                     String picture = objectFromArray.getString("strDrinkThumb");
+
                     String instructions = objectFromArray.getString("strInstructions");
 
                     String ingredient1 = objectFromArray.getString("strIngredient1");
@@ -88,8 +114,8 @@ public class CocktailActivity extends AppCompatActivity {
         //type 2
         public void onProgressUpdate(Integer ... args) {
 
-        }
 
+        }
         //type 3
         public void onPostExecute(String fromDoInBackground) {
             Log.i("HTTP", fromDoInBackground);
