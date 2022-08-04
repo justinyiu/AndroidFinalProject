48c4fddccc2585f60a4a75945fd4695f9bad067c
lian0094 <lian0094@algonquinlive.com>
2022-08-04 13:26:41 -0900

    yan-0803-1111

---
+++ app/src/main/java/com/cst2335/androidfinalproject/ListActivity.java
@@ -0,0 +1,12 @@
+package com.cst2335.androidfinalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cocktail);
+    }
+}
