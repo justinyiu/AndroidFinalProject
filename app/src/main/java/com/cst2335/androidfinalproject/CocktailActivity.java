8bf3c0352ba43d0da0bfa47d824ea34087567d68
justinyiu <yiu00005@algonquinlive.com>
2022-08-04 13:11:38 -0400

    Working on SQLite

--- app/src/main/java/com/cst2335/androidfinalproject/CocktailActivity.java
+++ app/src/main/java/com/cst2335/androidfinalproject/CocktailActivity.java
@@ -1,7 +1,10 @@
 package com.cst2335.androidfinalproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
@@ -19,6 +22,9 @@ import org.json.JSONObject;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.os.AsyncTask;
@@ -26,18 +32,31 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.Button;
 import android.content.Intent;
+import android.database.Cursor;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 
 public class CocktailActivity extends AppCompatActivity {
 
 
+    /**
+     * list of variables in the layout
+     */
+    private ListAdapter adapter;
+    private ListView theList; //id is theList
+    private ArrayList<HashMap<String, String>> cocktailList = new ArrayList<>();
+    //private MyListAdapter myAdapter;
     private EditText userText;
     private Button search;
-
-    /**
-     * database variables
-     */
-    private MyOpenHelper myOpener;
+    private MyOpenHelper myOpenHelper;
     private SQLiteDatabase myDatabase;
+    private String drinkSearch;
+
+    //TODO: make a progress bar
+    private ProgressBar cocktailProgressBar;
 
 
 
@@ -46,28 +65,110 @@ public class CocktailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail);
 
+        // find the id's
+        search = findViewById(R.id.search_button);
+        userText = findViewById(R.id.cocktailSearch);
+        theList = findViewById(R.id.theList);
+        cocktailProgressBar = findViewById(R.id.progressBar);
+
+        // TODO: make the snackbar
+        cocktailProgressBar.setVisibility(View.VISIBLE);
+
+        myOpenHelper = new MyOpenHelper(this);
+        myDatabase = myOpenHelper.getWritableDatabase();
+
+        /**
+         * send the query to the server
+         */
+        search.setOnClickListener(click->{
+            drinkSearch = userText.getText().toString();
+            drinkSearch = drinkSearch.replace(" ", "%20");
+            MyHttpRequest req = new MyHttpRequest();
+            req.execute();
+            cocktailProgressBar.setVisibility(View.VISIBLE);
+            userText.setText("");
+        });
+
+        theList.setOnItemClickListener((parent, view, position, id)->{
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(CocktailActivity.this)
+                    .setMessage("Do you want to view the details of this drink?").setTitle("View")
+                    .setCancelable(true)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            SQLiteDatabase myDatabase = myOpenHelper.getWritableDatabase();
+
+                            // will retrieve the hashmap that is stored in the array list
+                            HashMap<String, String> map = cocktailList.get(position);
+
+                            String [] strArray = new String[map.values().size()];
+                            int i = 0;
+
+                            for (String current : map.values()) {
+                                strArray[i] = current;
+                                i++;
+                            }
+
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put(myOpenHelper.COL_ID,strArray[0]);
+                            contentValues.put(myOpenHelper.COL_PICTURE,strArray[1]);
+                            contentValues.put(myOpenHelper.COL_INSTRUCTIONS,strArray[2]);
+                            contentValues.put(myOpenHelper.COL_INGREDIENTS,strArray[3]);
+                            myDatabase.insert(myOpenHelper.TABLE_NAME, "NullColumnName", contentValues);
+                            Toast toast = Toast.makeText(getApplicationContext(),R.string.Saved, Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    })
+                    .setNegativeButton((android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+            dialogBuilder.show();
+
+        });
+    }
+
+    //Override for options menu??
+
+    @Override
+            protected void onDestroy() {
+        super.onDestroy();
+        myOpenHelper.close();
+    }
+
+    @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+    }
+
+
+
+
+
+
+//**************************************************************************************************
         //TODO: add and initialize the database
         // inialize the database
         MyOpenHelper myOpener = new MyOpenHelper(this);
+        myDatabase = myOpener.getWritableDatabase();
+        Cursor cursor = myDatabase.rawQuery("Select * from " + MyOpenHelper.TABLE_NAME + ";", null);
 
 
         userText = findViewById(R.id.cocktailSearch);
         //EditText editText2 = findViewById(R.id.editText2);
-
-
         search = findViewById(R.id.search_button);
 
-
-
-        MyHttpRequest req = new MyHttpRequest();
+        //delete this later
+        //MyHttpRequest req = new MyHttpRequest();
 
 
 /**
  * When the user clicks the search button, it will get the user's text and execute the search query
- *
+ *Then it will launch into the DetailFragment class
+ * TODO: Add error handling: make sure the user only enters TEXT
  */
 
-//TODO: Add error handling
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,7 +182,9 @@ public class CocktailActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * establishes the HTTP request
+     */
 
     private class MyHttpRequest extends AsyncTask <String, Integer, String> {
 
@@ -142,6 +245,33 @@ public class CocktailActivity extends AppCompatActivity {
         //type 3
         public void onPostExecute(String fromDoInBackground) {
             Log.i("HTTP", fromDoInBackground);
+        }
+    }
+
+    public class Cocktail {
+        String instructions;
+        String ingredients;
+        long id;
+
+        public Cocktail(String instructions, String ingredients, long id) {
+            super();
+            this.instructions = instructions;
+            this.ingredients = ingredients;
+            this.id = id;
+        }
+
+        public String getInstructions() {
+            return this.instructions;
+        }
+
+        public String getIngredients() {
+            return this.ingredients;
+        }
+
+        public long getId() { return this.id;}
+
+        public String toString(){
+            return("message:" + this.instructions + " isSent:" + this.ingredients + " id:" + this.id);
         }
     }
 
