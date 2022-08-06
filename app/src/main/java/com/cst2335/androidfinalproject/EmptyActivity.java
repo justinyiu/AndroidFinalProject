e0cd2d179fd9311ef91cf20dc633297bf0a2d22a
Nicholas Calina <nicholascalina@gmail.com>
2022-08-06 18:31:57 -0400

    Committing to separate branch to limit confusion of changes

--- app/src/main/java/com/cst2335/androidfinalproject/EmptyActivity.java
+++ app/src/main/java/com/cst2335/androidfinalproject/EmptyActivity.java
@@ -10,5 +10,15 @@ public class EmptyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty);
+
+        Bundle dataToPass = getIntent().getExtras(); //get the data that was passed from FragmentExample
+
+        //This is copied directly from FragmentExample.java lines 47-54
+        DetailFragment dFragment = new DetailFragment();
+        dFragment.setArguments( dataToPass ); //pass data to the the fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentLocation, dFragment)
+                .commit();
     }
 }
